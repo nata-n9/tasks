543235521ce16ce670651db2b83294a341fe979a
Natalia Strelnikova <nata-n9@ya.ru>
2024-01-10 14:14:15 +0300

    Added Cl_2:Task3

---
+++ OneDrive/Рабочий стол/natasha/Cl_2/Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = 10;
+int[] arr = { 1, 4, 2, 6, 5, 9, 7, 8, 23, 64 };
+int i = 0;
+while (i < n)
+{
+    if (arr[i] % 2 == 0)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    i = i + 1;
+}
