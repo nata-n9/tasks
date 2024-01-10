73a1f8f07ce9b0d9bd735418857d63e9ba41b202
Natalia Strelnikova <nata-n9@ya.ru>
2024-01-10 15:07:33 +0300

    Added Task4

---
+++ OneDrive/Рабочий стол/natasha/Cl_2/Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int n = 5;
+int[] arr = { 2, 7, 1, 23, 11 };
+// int i = 0;
+// int max = arr[0];
+// while (i < arr.Length)
+// {
+//     if (arr[i] > max)
+//     {
+//         max = arr[i];
+//     }
+//     i = i + 1;
+// }
+//         Console.WriteLine(max);
+
+int max = arr[0];
+for (int j = 0; j < n; j++)
+    while (j < n)
+    {
+        if (arr[j] > max)
+        {
+            max = arr[j];
+        }
+    }
+Console.WriteLine(max);
+
