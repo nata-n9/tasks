7abb6971a0b4b7825f9f5966643cc7ff9a613c07
Natalia Strelnikova <nata-n9@ya.ru>
2024-01-15 18:23:11 +0300

    Added Task1

---
+++ OneDrive/Рабочий стол/natasha/Cl_3/Program.cs
@@ -0,0 +1,8 @@
+﻿double CalculateFormula(int a, int b, int c, int d);
+{
+    double numenator = a * b;
+    int denomenator = c + d;
+    double result = numenator / denomenator;
+    return result;
+}
+CalculateFormula(1, 2, 3, 4);
