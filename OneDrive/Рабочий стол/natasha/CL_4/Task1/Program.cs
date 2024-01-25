b93f943934fa271f1e39855d4754dfa2af70a94c
Natalia Strelnikova <nata-n9@ya.ru>
2024-01-25 10:07:49 +0300

    Added CL_4: Task2

--- OneDrive/Рабочий стол/natasha/CL_4/Task1/Program.cs
+++ OneDrive/Рабочий стол/natasha/CL_4/Task1/Program.cs
@@ -1,11 +1,10 @@
 ﻿// int[,] matrix = new int[3, 5];
-
-// Random rnd = new Random();
+//  Random rnd = new Random();
 // for (int i = 0; i < matrix.GetLength(0); i++)
 // {
 //     for (int j = 0; j < matrix.GetLength(1); j++)
 //     {
-
+       
 //         matrix[i, j] = rnd.Next(1, 11);
 //     }
 
@@ -18,7 +17,7 @@
 
 //         Console.Write($"{matrix[i, j]} ");
 //     }
-// Console.WriteLine();
+//     Console.WriteLine();
 // }
 
 
