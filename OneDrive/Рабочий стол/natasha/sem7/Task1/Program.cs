17d4d376e7cdd6f9302afebbd623f52f5ff40827
Natalia Strelnikova <nata-n9@ya.ru>
2024-02-07 00:31:50 +0300

    Added Task1

--- OneDrive/Рабочий стол/natasha/sem7/Task1/Program.cs
+++ OneDrive/Рабочий стол/natasha/sem7/Task1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1
+Console.WriteLine("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void PrintNumbersMN(int start, int end)
+{
+    if (end>start)
+    {
+        PrintNumbersMN(start, end-1);
+    }
+    Console.WriteLine(end);
+}
+if(m < n)
+
+{
+    PrintNumbersMN(m,n); 
+    }
+else
+{
+    PrintNumbersMN(n,m); }
